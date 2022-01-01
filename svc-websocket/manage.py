6b9a8a5719925734b6442259f2f5d24e277edbb2
ghozyghlmlaff <ghozyghlmlaff@gmail.com>
2022-01-01 13:38:00 +0700

    optimize websocket

--- svc-websocket/manage.py
+++ svc-websocket/manage.py
@@ -1,7 +1,7 @@
 # #!/usr/bin/env python3
 
 from flask import session, copy_current_request_context, render_template, request
-from flask_socketio import SocketIO, send, emit, disconnect
+from flask_socketio import SocketIO, send, disconnect
 from app import create_app
 from threading import Lock
 import os
@@ -20,16 +20,10 @@ socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins='*', ping_t
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-# @app.before_request
-# def make_session_permanent():
-#     session.permanent = True
-#     app.permanent_session_lifetime = timedelta(minutes=5)
-
 @socketio.on('connect_request')
 def connect_request(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+    send({'data': message['data'], 'count': session['receive_count']})
 
 @socketio.on('question')
 def question(message):
@@ -37,20 +31,11 @@ def question(message):
     payload = dict(question=message['data'], auth=os.getenv("SECRET_KEY"))
     r = requests.post(os.getenv("SVC_FLASK_ENDPOINT")+'/api/knowledge', data=payload)
     if isinstance(r.json()['data']['answer'],str):
-        emit('my_response', {'data': r.json()['data']['answer'], 'count': session['receive_count']})
+        send({'data': r.json()['data']['answer'], 'count': session['receive_count']})
     else:
         for row in r.json()['data']['answer']:
-            emit('my_response', {'data': row, 'count': session['receive_count']})
+            send({'data': row, 'count': session['receive_count']})
             session['receive_count'] = session.get('receive_count', 0) + 1
-
-
-@socketio.on('my_broadcast_event')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
 
 @socketio.on('disconnect_request')
 def disconnect_request():
@@ -59,8 +44,7 @@ def disconnect_request():
         disconnect()
 
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
+    send({'data': 'Disconnected!', 'count': session['receive_count']},
          callback=can_disconnect)
 
 if __name__ == '__main__':
